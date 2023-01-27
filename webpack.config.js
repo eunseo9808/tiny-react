46366eac3ecc81d265575a84ec1dd22fb3efc7e5
eunseo9808 <eunseo9808@naver.com>
2023-01-28 00:25:45 +0900

    feat: webpack initial setting

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    name: 'tiny-react-webpack-setting',
+    entry: "./src/index.ts",
+    output: {
+        path: path.join(__dirname, "build"),
+        filename: "index.js",
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react', "@babel/preset-typescript"]
+                    }
+                }
+            },
+            {
+                test: /\.tsx?$/,
+                exclude: /node_module/,
+                use: {
+                    loader: "ts-loader",
+                },
+            },
+        ],
+    },
+    resolve: {
+        modules: [path.join(__dirname, "src"), "node_modules"],
+        extensions: [".ts", ".tsx", ".js", ".jsx"],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html",
+        }),
+    ],
+    devServer: {
+        // host: "localhost",
+        static: './build',
+        port: 3000,
+    },
+    mode: "development",
+};
